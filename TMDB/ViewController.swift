8c0f244d23e72583011dc9fff15c25c41e087e96
Lavender <susie204@naver.com>
2023-09-02 23:41:26 +0900

    Initial Commit

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by 이승현 on 2023/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
